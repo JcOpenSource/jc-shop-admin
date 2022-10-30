f826d8cba03b479e7e7c0fa3c7d1344030c1185f
JcOpenSource <806959893@qq.com>
2022-10-30 18:54:01 +0800

    feat: reproject step03

--- src/layout/components/AppHeader/FullScreen.vue
+++ src/layout/components/AppHeader/FullScreen.vue
@@ -1,7 +1,20 @@
 <template>
-  <el-icon><FullScreen /></el-icon>
+  <el-icon @click="toggleFullScreen">
+    <FullScreen />
+  </el-icon>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+// toggle 触发
+const toggleFullScreen = () => {
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen()
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen()
+    }
+  }
+}
+</script>
 
 <style lang="scss" scoped></style>
